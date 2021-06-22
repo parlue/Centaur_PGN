57fdd8ed49c0372f6406212401af9ddc4dda497f
dsommerfeld <61017752+dsommerfeld@users.noreply.github.com>
2021-06-22 14:20:29 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 lichesstoken = "your-lichess-api-key-here"
-Emailaddress = "your@mail.com"
+emailaddress = "your@mail.com"
 smtpserver = "smpt.mail.com"
 smtpuser = "name@name.com"
 smtp_encryption = "True"
 smtppassword = "hidden"
-Subject = "New PGN file from Centaur"
+subject = "New PGN file from Centaur"
 

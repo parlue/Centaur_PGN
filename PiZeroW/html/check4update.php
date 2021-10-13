abe1b8dc6afb1c9cd32cb045df0c9741c04f6797
Digitsoftware <b.aldag@digitsoftware.de>
2021-10-13 16:09:59 +0200

    Add files via upload

--- PiZeroW/html/check4update.php
+++ PiZeroW/html/check4update.php
@@ -1,11 +1,11 @@
 <?php
 
-exec('sudo wget https://serverurlkommtnoch/cent/update/centupdate.gz');
-$existing_file = '/update/centupdate.gz';
-$new_file = '/tmp/centupdate.gz';
+exec('sudo wget https://serverurlkommtnoch/cent/update/centupdate.gzip');
+$existing_file = '/update/centupdate.gzip';
+$new_file = '/tmp/centupdate.gzip';
 $haschmich = 'md5';
 
-if (hash_file($algo, $existing_file) === hash_file($algo, $new_file)) {
+if (hash_file($haschmich, $existing_file) === hash_file($hashmich, $new_file)) {
 
 //blablablub.....Update-Routine muss hier rein.
 

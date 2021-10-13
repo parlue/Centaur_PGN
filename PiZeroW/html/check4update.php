c0765d909c6f6f029c390691ed09d69e4fc98179
Digitsoftware <b.aldag@digitsoftware.de>
2021-10-13 16:08:30 +0200

    Update check4update.php

--- PiZeroW/html/check4update.php
+++ PiZeroW/html/check4update.php
@@ -1,8 +1,8 @@
 <?php
 
-exec('sudo wget https://serverurlkommtnoch/cent/update/centupdate.gzip');
-$existing_file = '/update/centupdate.gzip';
-$new_file = '/tmp/centupdate.gzip';
+exec('sudo wget https://serverurlkommtnoch/cent/update/centupdate.gz');
+$existing_file = '/update/centupdate.gz';
+$new_file = '/tmp/centupdate.gz';
 $haschmich = 'md5';
 
 if (hash_file($algo, $existing_file) === hash_file($algo, $new_file)) {

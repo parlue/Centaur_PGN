b13fa9fa2351a0805c22a6759f21b3e612dac64d
dsommerfeld <61017752+dsommerfeld@users.noreply.github.com>
2021-11-30 10:14:22 +0100

    add new terms

--- PiZeroW/home/pi/v2/board/board.py
+++ PiZeroW/home/pi/v2/board/board.py
@@ -1,9 +1,25 @@
 # DGT Centaur board control functions
 #
-# I am not really a python programmer, but the language choice here
-# made sense!
+# This file is part of the DGTCentaur Mods open source software
+# ( https://github.com/EdNekebno/DGTCentaur )
 #
-# Ed Nekebno
+# DGTCentaur Mods is free software: you can redistribute
+# it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# DGTCentaur Mods is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this file.  If not, see
+#
+# https://github.com/EdNekebno/DGTCentaur/blob/master/LICENSE.md
+#
+# This and any other notices must remain intact and unaltered in any
+# distribution, modification, variant, or derivative of this software.
 
 import serial
 import sys
